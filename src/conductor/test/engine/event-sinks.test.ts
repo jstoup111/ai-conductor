b24380fd2e31a86c7a56c8cf7f36b133ed26d238
James Stoup <jstoup111@users.noreply.github.com>
2026-09-06 15:04:36 -0400

    test(build-review): repair frozen source fixtures

--- src/conductor/test/engine/event-sinks.test.ts
+++ src/conductor/test/engine/event-sinks.test.ts
@@ -147,6 +147,7 @@ const PINNED_PERSISTED_EVENT_TYPES = [
   'self_host_containment_verdict',
   'over_scope_decision',
   ...REMEDIATION_CASE_LIFECYCLE_EVENT_TYPES,
+  'build_review_scope_incomplete',
 ] satisfies Array<ConductorEvent['type']>;
 
 const NON_PERSISTED_REBASE_LIFECYCLE_EVENT_TYPES = [
@@ -215,6 +216,7 @@ const DAEMON_SWITCH_HANDLED_EVENT_TYPES = [
   'rebase_conflict_halt',
   'ci_failed',
   'build_review_base',
+  'build_review_scope_incomplete',
   'build_review_stale_mirage_regrade',
   'auto_park_contradiction',
   'verdict_freshness',
